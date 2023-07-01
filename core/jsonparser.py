edae8c33c3d53f4afcc25628b8e735c0283a2ac9
SungJin Lee <lsj000424@gmail.com>
2023-07-01 20:10:23 +0000

    [FIX] for exercise app

--- core/jsonparser.py
+++ core/jsonparser.py
@@ -2,16 +2,15 @@ import os
 import json
 import logging
 
-jsonfile = os.path.join("", 'settings.json')
-with open(jsonfile, 'r') as f:
-    values = json.loads(f.read())
- 
-def getJsonValue(key):
-        try:
-            return values[key]
-        except KeyError:
-            error_msg = "Set the {} environment variable".format(key)
-            raise KeyError(error_msg)
+def getJsonValue(key, filename='settings.json'):
+    jsonfile = os.path.join("", filename)
+    with open(jsonfile, 'r') as f:
+        values = json.loads(f.read())
+    try:
+        return values[key]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(key)
+        raise KeyError(error_msg)
 
 if __name__ == "__main__":
-    print(getJsonValue("discordkey"))
+    print()
